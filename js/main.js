1a605c09bc0d5ba28ed4bdf05e1dae599733fded
Nima2800 <nima12345693@gmail.com>
2023-08-28 18:11:01 +0330

    page 2

--- js/main.js
+++ js/main.js
@@ -5,9 +5,17 @@ function scrollFunction() {
     document.getElementById("nav").className = "nav sticky";
     document.getElementById("logo-w").className = "none";
     document.getElementById("logo").className = "logo";
+    document.getElementById("menu-line").className = "line-black";
+    document.getElementById("menu-line1").className = "line-black";
+    document.getElementById("menu-line2").className = "line-black";
+
   } else {
     document.getElementById("nav").className = "nav";
     document.getElementById("logo-w").className = "logo";
     document.getElementById("logo").className = "none";
+    document.getElementById("menu-line").className = "line";
+    document.getElementById("menu-line1").className = "line";
+    document.getElementById("menu-line2").className = "line";
+
   }
 }
